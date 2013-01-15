c2d687a8e2a9a69028324c97e813d45791b01304
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2013-01-15 13:03:02 +0100

    cleaned up not necessary rtt stuff

--- bride_plugin_source/src/org.best_of_robotics.model.bcm.diagram/src/org/best_of_robotics/model/bcm/diagram/edit/helpers/BundleEditHelper.java
+++
@@ -1,8 +0,0 @@
-package org.best_of_robotics.model.bcm.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class BundleEditHelper extends
-		org.best_of_robotics.model.bcm.diagram.edit.helpers.BcmBaseEditHelper {
-}
--- bride_plugin_source/src/org.best_of_robotics.model.orocos.diagram/src/org/best_of_robotics/model/orocos/diagram/edit/helpers/PropertyEditHelper.java
+++
@@ -1,9 +0,0 @@
-package org.best_of_robotics.model.orocos.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class PropertyEditHelper
-		extends
-		org.best_of_robotics.model.orocos.diagram.edit.helpers.OrocosBaseEditHelper {
-}
