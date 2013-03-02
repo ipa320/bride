996dc07d31f19f0c8f00d1e1f207612ee4802480
Alexander Bubeck <mail@cybernotic.org>
2013-03-02 20:15:24 +0100

    improved standalone m2t compiler

--- bride_compilers/src/M2CStandalone.java
+++ bride_compilers/src/M2CStandalone.java
@@ -1,7 +1,12 @@
 
 import java.net.URI;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException; 
+import java.util.ArrayList;
+import java.util.List;
 	
 
 
@@ -44,8 +49,8 @@ public class M2CStandalone {
 		EmfModel emfModel = new EmfModel();
 		StringProperties properties = new StringProperties();
 		properties.put(EmfModel.PROPERTY_NAME, name);
-		properties.put(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI, "file:/" + getFile(metamodel).getAbsolutePath());
-		properties.put(EmfModel.PROPERTY_MODEL_URI, "file:/" + getFile(model).getAbsolutePath());
+		properties.put(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI, "file:/" + new File(metamodel).getAbsolutePath());
+		properties.put(EmfModel.PROPERTY_MODEL_URI, "file:/" + new File(model).getAbsolutePath());
 		properties.put(EmfModel.PROPERTY_IS_METAMODEL_FILE_BASED, "true");
 		properties.put(EmfModel.PROPERTY_READONLOAD, readOnLoad + "");
 		properties.put(EmfModel.PROPERTY_STOREONDISPOSAL, storeOnDisposal + "");
@@ -53,33 +58,70 @@ public class M2CStandalone {
 		return emfModel;
 	}
 	
+	public static String findPackageURI(String packagename)
+	{
+		String cmd = "rospack find " + packagename;
+		Runtime run = Runtime.getRuntime();
+		Process pr;
+		try {
+			pr = run.exec(cmd);
+			pr.waitFor();
+			BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			return buf.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
 	 public static void main(String[] args) {
-	        try {
-	        	EglFileGeneratingTemplateFactory eglTemplateFactory = new EglFileGeneratingTemplateFactory();
-	        	IEolExecutableModule module = new EglTemplateFactoryModuleAdapter(eglTemplateFactory);
-	        	System.out.println("Parsing template files...");
-	        	eglTemplateFactory.setOutputRoot(M2CStandalone.class.getResource("ros_package/").toString());
-	        	module.parse(getFile("egl/package.egl"));
-	        	
-	        	System.out.println("Creating final model out of dsl and specific model...");
-	        	EmfModel model = createEmfModel("Model", "models/gripper.ros", "models/ros.ecore", true, false);
-				
-				System.out.println("Adding model to template egl engine...");
-	        	module.getContext().getModelRepository().addModel(model);
-	        	
-	        	Object result = new Object();
-	        	try {
-	        		System.out.println("Executing engine...");
-	    			result = module.execute();
-	    		} catch (EolRuntimeException e) {
-	    			e.printStackTrace();
-	    		}
-	        	System.out.println(result);
-	        	System.out.println("Done.");
-	        } catch (Throwable t) {
-	            System.err.println("An unexpected error occured");
-	            t.printStackTrace();
-	        }
+		 	if(args.length == 1)
+		 	{
+		        try {
+		        	// Try to find template entrance point
+		        	String template_uri = findPackageURI("bride_templates") + "/cpp/package.egl";
+		        	System.out.println("Will use template from " + template_uri);
+		        	// Try to find model file
+		        	String model_uri = findPackageURI(args[0]) + "/model/" + args[0] + ".ros_package";
+		        	System.out.println("Will use model from " + model_uri);
+		        	// Try to find output directory
+		        	String output_uri = findPackageURI(args[0]);
+		        	System.out.println("Will generate code to " + output_uri);
+		        	
+		        	EglFileGeneratingTemplateFactory eglTemplateFactory = new EglFileGeneratingTemplateFactory();
+		        	IEolExecutableModule module = new EglTemplateFactoryModuleAdapter(eglTemplateFactory);
+		        	System.out.println("Parsing template files...");
+		        	eglTemplateFactory.setOutputRoot(output_uri);
+		        	module.parse(new File(template_uri));
+		        	
+		        	System.out.println("Creating final model out of dsl and specific model...");
+		        	EmfModel model = createEmfModel("Model", model_uri, findPackageURI("bride_compilers")+"/bin/models/ros.ecore", true, false);
+					
+					System.out.println("Adding model to template egl engine...");
+		        	module.getContext().getModelRepository().addModel(model);
+		        	
+		        	Object result = new Object();
+		        	try {
+		        		System.out.println("Executing engine...");
+		    			result = module.execute();
+		    		} catch (EolRuntimeException e) {
+		    			e.printStackTrace();
+		    		}
+		        	System.out.println(result);
+		        	System.out.println("Done.");
+		        } catch (Throwable t) {
+		            System.err.println("An unexpected error occured");
+		            t.printStackTrace();
+		        }
+		 	}
+		 	else
+		 	{
+		 		System.out.println("Usage: m2t package_name ");
+		 	}
 	 }
 	
 }
