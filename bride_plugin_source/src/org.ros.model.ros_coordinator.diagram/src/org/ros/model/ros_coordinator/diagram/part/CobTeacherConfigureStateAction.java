157819ac1728eda8d9fb5bfa324ea19cffe3b417
abubeck <alexander.bubeck@ipa.fraunhofer.de>
2013-10-25 10:24:04 +0200

    Updated templates. Changed launching of cob_teacher.

--- bride_plugin_source/src/org.ros.model.ros_coordinator.diagram/src/org/ros/model/ros_coordinator/diagram/part/CobTeacherConfigureStateAction.java
+++ bride_plugin_source/src/org.ros.model.ros_coordinator.diagram/src/org/ros/model/ros_coordinator/diagram/part/CobTeacherConfigureStateAction.java
@@ -3,12 +3,16 @@ package org.ros.model.ros_coordinator.diagram.part;
 import java.io.IOException;
 import java.util.Map;
 
-
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 import org.ros.model.ros_coordinator.diagram.edit.parts.ActionStateEditPart;
 import org.ros.model.ros_coordinator.diagram.edit.parts.ActionStateNameEditPart;
 
@@ -24,9 +28,19 @@ public class CobTeacherConfigureStateAction implements IObjectActionDelegate {
 		if (selectedElement == null) {
 			return;
 		}
+		String config_file = "";
 		String nodename = ((ActionStateNameEditPart)selectedElement.getChildBySemanticHint(SmachVisualIDRegistry.getType(5001))).getEditText();
+		IFile sourcefile = null;
+		IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if (editor != null) {
+            IEditorInput input = editor.getEditorInput();
+            if (input instanceof IFileEditorInput) {
+            	sourcefile = ((IFileEditorInput)input).getFile();
+            	config_file = sourcefile.getParent().getParent().getLocation().toOSString() + "/config/" + nodename+"_goal.yaml";
+            }
+		}
 		
-		String cmd = "rosrun cob_teacher bride_teacher " + nodename;
+		String cmd = "rosrun cob_teacher bride_teacher " + config_file;
 		System.out.println(cmd);
 		Runtime run = Runtime.getRuntime();
 		Process pr;
