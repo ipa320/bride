8594effb056e1e8086f9ff937351d5e1600dd565
Alexander Bubeck <mail@cybernotic.org>
2014-04-05 10:23:40 +0200

    checking for errors in model before generating code. Should fix #41

--- bride_plugin_source/src/org.best_of_robotics.transform.ros.to.cplusplus/src/org/best_of_robotics/transform/ros/to/cplusplus/handler/RosPythonTransform.java
+++ bride_plugin_source/src/org.best_of_robotics.transform.ros.to.cplusplus/src/org/best_of_robotics/transform/ros/to/cplusplus/handler/RosPythonTransform.java
@@ -17,6 +17,9 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
@@ -51,6 +54,22 @@ public class RosPythonTransform extends AbstractHandler {
 							"Error on Editor Selection 2",
 							"Please select the editor from which you want to generate code and execute command again.");
             	}
+            	try {
+					int errors = sourcefile.getProject().findMaxProblemSeverity(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+					if(errors != -1)
+					{
+						sourcefile = null;
+	            		MessageDialog
+						.openError(
+								PlatformUI.getWorkbench()
+										.getActiveWorkbenchWindow().getShell(),
+								"Model problem found",
+								"Your model is not valid yet. Please fix the issues you find in the Problems tab before generating code out of this model.");
+					}
+                } catch (CoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
                 
             }
         }
